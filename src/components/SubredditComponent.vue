6cc0610289b1c250b7e9ce64391fb2ee6c7eab8e
Joseph Moran III <jmoran@clarkinc.biz>
2018-02-07 11:44:36 -0500

    Fix card title format

--- src/components/SubredditComponent.vue
+++ src/components/SubredditComponent.vue
@@ -1,26 +1,35 @@
 <template>
   <v-flex v-if="stream" class="pa-1" xs12 sm6 md4>
     <v-card class="subreddit">
-      <v-card-title :class="[status, 'darken-1', 'subreddit']">
-        <v-icon class="white--text pl-2">{{ streamTypeIcon }}</v-icon>
-        <v-spacer></v-spacer>
-        <a :href="subredditLink" target="_blank" class="white--text subreddit-header headline">{{ subreddit.name }}</a>
-        <v-spacer></v-spacer>
-        <div>
-          <v-menu bottom left origin="top right">
-            <v-btn icon="icon" slot="activator" class="white--text">
-              <v-icon>more_vert</v-icon>
-            </v-btn>
-            <v-list class="pa-0">
-              <v-list-tile :to="{ name: 'Edit Subreddit', params:{ id: index }}">
-                <v-list-tile-title class="grey--text text--darken-3">Edit Subreddit</v-list-tile-title>
-              </v-list-tile>
-              <v-list-tile @click="$store.dispatch('removeStream', { index })">
-                <v-list-tile-title>Remove Card</v-list-tile-title>
-              </v-list-tile>
-            </v-list>
-          </v-menu>
-        </div>
+      <v-card-title class="subreddit">
+        <v-toolbar :class="[status, 'darken-1']">
+          <v-toolbar-title>
+            <v-icon class="white--text pl-2">{{ streamTypeIcon }}</v-icon>
+          </v-toolbar-title>
+          <v-spacer></v-spacer>
+          <v-toolbar-title class="white--text">
+            <a :href="subredditLink" target="_blank" class="white--text subreddit-header headline">{{ subreddit.name }}</a>
+          </v-toolbar-title>
+          <v-spacer></v-spacer>
+          <v-toolbar-title>
+            <div>
+              <v-menu bottom left origin="top right">
+                <v-btn icon="icon" slot="activator" class="white--text">
+                  <v-icon>more_vert</v-icon>
+                </v-btn>
+                <v-list class="pa-0">
+                  <v-list-tile :to="{ name: 'Edit Subreddit', params:{ id: index }}">
+                    <v-list-tile-title class="grey--text text--darken-3">Edit Subreddit</v-list-tile-title>
+                  </v-list-tile>
+                  <v-list-tile @click="$store.dispatch('removeStream', { index })">
+                    <v-list-tile-title>Remove Card</v-list-tile-title>
+                  </v-list-tile>
+                </v-list>
+              </v-menu>
+            </div>
+          </v-toolbar-title>
+        </v-toolbar>
+        
       </v-card-title>
       <v-card-text class="article-list-container">
         <v-list class="article-list" three-line dense>
